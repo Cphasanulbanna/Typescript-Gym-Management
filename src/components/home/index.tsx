b1dbc700c3f98d2818e3aa54211004f718e92aa1
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-18 02:36:53 +0530

    framermotion animations addded

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,6 +1,9 @@
 //ENUM TYPE
 import { SelectedPage } from "@/shared/types";
-4;
+
+//PAACKAGES
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 //COMPONENTS
 import ActionButton from "@/shared/ActionButton";
@@ -14,35 +17,57 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
-import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Home = (setSelectedPage: Props) => {
+const Home = ({ setSelectedPage }: Props) => {
   const iseAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* IMAGE & HEADER SECTION */}
-      <div>
+      <motion.div
+        onClick={() => setSelectedPage(SelectedPage.Home)}
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+      >
         {/* MAIN HEADER */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5 ">
           {/* HEADINGS */}
-          <div>
-            <div>
-              <div>
+          <motion.div
+            initial="hidden"
+            whileInView={"visible"}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            className="md:-mt-20"
+          >
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
               </div>
             </div>
-            <p>
+            <p className="mt-8 text-sm ">
               Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
             </p>
-          </div>
+          </motion.div>
           {/* BUTTONS */}
-          <div>
+          <motion.div
+            initial="hidden"
+            whileInView={"visible"}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            className="mt-8 flex items-center gap-8"
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -53,19 +78,19 @@ const Home = (setSelectedPage: Props) => {
             >
               <p>Leran More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
 
         {/* IMAGE */}
-        <div>
+        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-end ">
           <img src={HomePageGraphic} alt="home-page-graphic" />
         </div>
-      </div>
+      </motion.div>
       {/* SPONSORS */}
       {iseAboveMediumScreens && (
-        <div>
-          <div>
-            <div>
+        <div className="h-[150] w-full bg-primary-500 py-10">
+          <div className="mx-auto w-5/6 ">
+            <div className="w-/5 flex items-center justify-between gap-8">
               <img src={SponsorRedBull} alt="redbull" />
               <img src={SponsorForbes} alt="forbs" />
               <img src={SponsorFortune} alt="fortune" />
